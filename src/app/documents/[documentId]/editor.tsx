26ff54013265998cb24135e47ee6dcdb9a85ec61
vickydartech <73622790+vickyalvandob@users.noreply.github.com>
2025-03-10 15:31:09 +0700

    add tiptap editor

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
+
+export const Editor = () => {
+    const editor = useEditor({
+        editorProps: {
+            attributes: {
+                style: "padding-left: 56px; padding-right:56px",
+                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
+            },
+        },
+        extensions: [
+            StarterKit,
+            TaskItem.configure({
+                nested: true,
+            }),
+            TaskList,
+            Table.configure({
+                resizable: true,
+              }),
+            TableRow,
+            TableHeader,
+            TableCell,
+            Image,
+            ImageResize,
+        ],
+        content: `
+        <p>Hello World! 🌎️</p>
+
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
+      })
+    
+    
+    return (
+        <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+                <EditorContent editor={editor} />
+            </div>
+        </div>
+    );
+}
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocumentIdPageProps {
     params: Promise<{documentId: string}>;
 };
@@ -8,8 +10,9 @@ const DocumentIdPage = async ({params} : DocumentIdPageProps) => {
     const {documentId} = await params;
     
     return ( 
-        <div>
-            Document ID: { documentId }
+        <div className="min-h-screen bg-[#FAFBFD]">
+         
+            <Editor/>
         </div>
      );
 }
