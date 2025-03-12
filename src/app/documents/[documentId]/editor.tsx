53ff1c917f2d6be324c6a62c2d19706b05bf2f5c
vickydartech <73622790+vickyalvandob@users.noreply.github.com>
2025-03-12 13:57:43 +0700

    add ruler

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -18,15 +18,19 @@ import Highlight from '@tiptap/extension-highlight'
 import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
 
-
 import {useEditorStore} from '@/store/use-editor-store'
 import { FontSizeExtension } from '@/extensions/font-size'
+import { LineHeightExtension } from '@/extensions/line-height'
+
+import { Ruler } from './ruler'
+
 
 
 export const Editor = () => {
     const {setEditor} = useEditorStore();
 
     const editor = useEditor({
+        immediatelyRender: false,
         onCreate({editor}){
             setEditor(editor);
         },
@@ -87,6 +91,10 @@ export const Editor = () => {
                 types: ["heading", "paragraph"],
             }),
             FontSizeExtension,
+            LineHeightExtension.configure({
+                types: ["heading", "paragraph"],
+                defaultLineHeight: "normal"
+            }),
         ],
         content: `
         <p>Hello World! 🌎️</p>
@@ -111,6 +119,7 @@ export const Editor = () => {
     
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler />
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
                 <EditorContent editor={editor} />
             </div>
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -4,7 +4,7 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { AlignCenterIcon, AlignJustifyIcon, AlignLeftIcon, AlignRightIcon, BoldIcon, ChevronDownIcon, HighlighterIcon, Icon, ImageIcon, ItalicIcon, Link2Icon, ListIcon, ListOrderedIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, MinusIcon, PlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SearchIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon, UploadIcon } from "lucide-react";
+import { AlignCenterIcon, AlignJustifyIcon, AlignLeftIcon, AlignRightIcon, BoldIcon, ChevronDownIcon, HighlighterIcon, Icon, ImageIcon, ItalicIcon, Link2Icon, ListCollapseIcon, ListIcon, ListOrderedIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, MinusIcon, PlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SearchIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon, UploadIcon } from "lucide-react";
 import { type Level } from "@tiptap/extension-heading";
 import { ColorResult, SketchPicker } from "react-color";
 import { useState } from "react";
@@ -13,6 +13,62 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import TextAlign from "@tiptap/extension-text-align";
 
+
+
+const LineHeightButton = () => {
+    const {editor} = useEditorStore();
+
+    const lineHeights = [
+        {
+            label: "Default",
+            value: "normal",
+        },
+        {
+            label: "Single",
+            value: "1",
+        },
+        {
+            label: "1.15",
+            value: "1.15",
+        },
+        {
+            label: "1.5",
+            value: "1.5",
+        },
+        {
+            label: "Double",
+            value: "2",
+        }
+    ];
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                   <ListCollapseIcon className="size-4" />
+               
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent >
+                {lineHeights.map(({label, value}) => (
+                    <button 
+                    key={value}
+                    onClick={() => editor?.chain().focus().setLineHeight(value).run()}
+                    className={cn(
+                        "w-full flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                        editor?.getAttributes("paragraph").lineHeight == value && "bg-neutral-200/80"
+                    )}
+                    >
+                       
+                        <span className="text-sm">{label}</span>
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 
 
 const FontSizeButton = () => {
@@ -129,13 +185,13 @@ const ListButton = () => {
                
                 </button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="p-0">
+            <DropdownMenuContent>
                 {lists.map(({label, icon: Icon, onClick, isActive}) => (
                     <button 
                     key={label}
                     onClick={onClick}
                     className={cn(
-                        "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                        "w-full flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
                        isActive() && "bg-neutral-200/80"
                     )}
                     >
@@ -184,13 +240,13 @@ const AlignButton = () => {
                
                 </button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="p-0">
+            <DropdownMenuContent >
                 {alignments.map(({label, value, icon: Icon}) => (
                     <button 
                     key={value}
                     onClick={() => editor?.chain().focus().setTextAlign(value).run()}
                     className={cn(
-                        "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                        "w-full flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
                         editor?.isActive({TextAlign: value}) && "bg-neutral-200/80"
                     )}
                     >
@@ -502,7 +558,7 @@ const FontFamilyButton = () => {
                     onClick={() => editor?.chain().focus().setFontFamily(value).run()}
                     key={value}
                     className={cn(
-                        "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                        "w-full flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
                         editor?.getAttributes("textStyle").fontFamily == value && "bg-neutral-200/80"
                     )}
                     style={{fontFamily: value}}
@@ -641,6 +697,7 @@ export const Toolbar = () => {
             <LinkButton />
             <ImageButton />
             <AlignButton />
+            <LineHeightButton />
             <ListButton />
 
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
